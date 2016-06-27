0ae8b82e76aa4aa28ad4e9839b4aa62a1e939e56
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-06-27 12:47:24 -0500

    Remove default test

--- test/benchmark/inputs_test.rb
+++ test/benchmark/inputs_test.rb
@@ -4,8 +4,4 @@ class Benchmark::InputsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Benchmark::Inputs::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
